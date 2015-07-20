05f1463af6e8eeae77dcac96203982d213660d4a
Michael Aufreiter <michael@substance.io>
2015-07-20 23:22:14 +0200

    Upgrade to new Lens.

--- lens.js
+++ lens.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var Lens = require("lens");
+var Lens = require("lens/reader");
 var panels = Lens.getDefaultPanels();
   
 // All available converters
-var LensConverter = require("lens-converter");
-var ElifeConverter = require("lens-converter/elife_converter");
+var LensConverter = require("lens/converter");
+var ElifeConverter = require("lens/converter/elife_converter");
 
 var LensApp = function(config) {
   Lens.call(this, config);
@@ -19,7 +19,7 @@ LensApp.Prototype = function() {
   // Provides a sequence of converter instances
   // Converter.match will be called on each instance with the
   // XML document to processed. The one that returns true first
-  // will be chosen. You can change the order prioritize
+  // will be chosen. You can change the order to prioritize
   // converters over others
 
   this.getConverters = function(converterOptions) {
--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var _ = require("underscore");
 var fs = require('fs');
+var path = require("path");
 var ejs = require('ejs');
 
-var path = require("path");
-var CJSServer = require('substance-cjs');
+var sass = require('node-sass');
+var browserify = require('browserify');
 
 var app = express();
 
@@ -15,56 +16,61 @@ app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname);
-
 var SAMPLESET = require('./sampleset');
 
 app.get("/",
   function(req, res, next) {
     ejs.renderFile(__dirname+'/sampleset.ejs', { sampleset: SAMPLESET }, function(err, result) {
       if (err) return next(err);
-      // if (fs.existsSync('dist')){
-      //   fs.writeFileSync('dist/index.html', result);
-      // }
       res.send(result);
     });
   }
 );
 
 
-var config = require("./.screwdriver/project.json");
-
-
-new CJSServer(app, __dirname, 'lens')
-  // ATTENTION: the second argument is the script which is resembled by injecting a list
-  // of script tags instead. It must be exactly the same string which is used in the script src.
-  .scripts('./boot.js', 'lens.js', {
-    ignores: [
-      'substance-commander',
-      'substance-chronicle',
-      'substance-operator'
-    ]
-  })
-  // ... the same applies to the css file
-  .styles(config.styles, 'lens.css')
-  .page('/doc.html', './doc.html');
-
-// Serve assets with alias as configured in project.json (~dist like setup)
-_.each(config.assets, function(srcPath, distPath) {
-  var absPath = path.join(__dirname, srcPath);
-  var route = "/" + distPath;
-  //console.log("Adding route for asset", route, "->", absPath);
-  if (fs.lstatSync(absPath).isDirectory()) {
-    app.use( route, express.static(absPath) );
-  } else {
-    app.use( route, function(req, res) {
-      res.sendfile(absPath);
-    } );
-  }
+app.get('/lens.js', function (req, res, next) {
+  browserify({ debug: true, cache: false })
+    .add(path.join(__dirname, "boot.js"))
+    .bundle()
+    .on('error', function(err, data){
+      console.error(err.message);
+      res.send('console.log("'+err.message+'");');
+    })
+    .pipe(res);
 });
 
+var handleError = function(err, res) {
+  console.error(err);
+  res.status(400).json(err);
+};
+
+var renderSass = function(cb) {
+  sass.render({
+    file: path.join(__dirname, "lens.scss"),
+    sourceMap: true,
+    outFile: 'lens.css',
+  }, cb);
+};
+
+app.get('/lens.css', function(req, res) {
+  renderSass(function(err, result) {
+    if (err) return handleError(err, res);
+    res.set('Content-Type', 'text/css');
+    res.send(result.css);
+  });
+});
+
+app.get('/lens.css.map', function(req, res) {
+  renderSass(function(err, result) {
+    if (err) return handleError(err, res);
+    res.set('Content-Type', 'text/css');
+    res.send(result.map);
+  });
+});
+
+// Serve files from root dir
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'assets')));
 
 // Serve Lens in dev mode
 // --------
