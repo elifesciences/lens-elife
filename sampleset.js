7fc6c09c5562e559c2adcfb73ce0a99b0cd8189b
Michael Aufreiter <michael@substance.io>
2015-10-08 15:41:34 +0200

    Include new quote node type.

--- sampleset.js
+++ sampleset.js
@@ -17,6 +17,10 @@ module.exports = [
       {
         "url": "https://s3.amazonaws.com/elife-cdn/elife-articles/03785/elife03785.xml",
         "comment": ""
+      },
+      {
+        "url": "/example-with-quotes.xml",
+        "comment": ""
       }
     ]
   }
