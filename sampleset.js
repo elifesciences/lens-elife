379033405bbfe5b5360547d3f7fb03a7ffbcd991
Graham Nott <gnott@starglobal.ca>
2019-12-05 17:48:16 -0800

    Update sample set of eLIfe XML files.

--- sampleset.js
+++ sampleset.js
@@ -3,15 +3,19 @@ module.exports = [
     "name": "eLife Samples",
     "samples": [
       {
-        "url": "https://s3.amazonaws.com/elife-publishing-cdn/00311/elife-00311-v1.xml",
+        "url": "https://s3.amazonaws.com/prod-elife-published/articles/00007/elife-00007-v1.xml",
         "comment": ""
       },
       {
-        "url": "https://s3.amazonaws.com/elife-publishing-cdn/00778/elife-00778-v1.xml",
+        "url": "https://s3.amazonaws.com/prod-elife-published/articles/00311/elife-00311-v1.xml",
         "comment": ""
       },
       {
-        "url": "https://s3.amazonaws.com/elife-publishing-cdn/15278/elife-15278-v1.xml",
+        "url": "https://s3.amazonaws.com/prod-elife-published/articles/00778/elife-00778-v1.xml",
+        "comment": ""
+      },
+      {
+        "url": "https://s3.amazonaws.com/prod-elife-published/articles/15278/elife-15278-v1.xml",
         "comment": ""
       },
       {
